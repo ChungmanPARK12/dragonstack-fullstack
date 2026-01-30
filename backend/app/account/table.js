37a2716b85fa25cb4c287489de377195bca58ed9
root <root@DESKTOP-NFQ3D4M.localdomain>
2026-01-31 08:08:13 +1030

    166-Update-balance

--- backend/app/account/table.js
+++ backend/app/account/table.js
@@ -41,6 +41,19 @@ class AccountTable {
             )
         });
     }
+
+    static updateBalance({ accountId, value }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                'UPDATE account SET balance = balance + $1 WHERE id = $2',
+                [accountId, value],
+                (error, response) => {
+                    if (error) return reject(error);
+                    resolve();
+                }
+            )
+        });
+    }
 }
 
 module.exports = AccountTable;
